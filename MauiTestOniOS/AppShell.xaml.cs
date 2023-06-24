744c5ffaa211094cbd165ac6b19893e358dbc77f
damnjan <damnjan.markovic@hotmail.com>
2023-06-24 13:36:48 +0200

    FirstCommit

---
+++ MauiTestOniOS/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTestOniOS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiTestOniOS/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiTestOniOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
